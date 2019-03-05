149618dec4f01e00b74df07095e5204ef6ba316a
Fozlpok <Fozlpok@gmail.com>
2019-03-05 18:34:04 +0800

    hw1

--- main.c
+++ main.c
@@ -5,19 +5,30 @@ int plus(int a, int b) { return a + b; }
 int minus(int a, int b) { return a - b; }
 int multiply(int a, int b) { return a * b; }
 int divided(int a, int b) { return a / b; }
+
+int (*a[4]) (int x, int y);
+
 int main()
 {
-	int a, c;
-	char b;
-	printf("key a Function \nEX: 1 + 1\n");
-	scanf("%i %c %i", &a, &b, &c);
-	//IF ELSE IF
-	if (b == '+')
-		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
-	else if (b == '-')
-		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
-	else if (b == '*')
-		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
-	else if (b == '/')
-		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+	int result;//set variable
+  	int i, j, tep;
+	a[0] = plus; 
+  	a[1] = minus; 
+  	a[2] = multiply;
+  	a[3] = divided;
+	result = (*a[tep]) (i, j);
+	
+	printf("Enter two numbers: ");
+  	scanf("%d %d", &i, &j);
+  
+  	printf("0: plus, 1: minus, 2: multiply, 3: divided\n");
+  	do {
+    	printf("enter number of operation: ");
+    	scanf("%d", &tep);
+  	} while(tep<0 || tep>3);
+
+  	result = (*a[tep]) (i, j);
+  	printf("%d", result);
+
+ 	return 0;
 }
